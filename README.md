13b586e309b4a173b07684460c8a2fbd4a17591d
vini1994 <vinirabaioli94@gmail.com>
2026-02-21 22:29:14 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Estudo curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
-
-    return <h1>Anoveu, Te ambrus, ti ambrus, ti ambrus! Anouveus!</h1>
+  return <h1>Anoveu, Te ambrus, ti ambrus, ti ambrus! Anouveus!</h1>;
 }
 
 export default Home;
