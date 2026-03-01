92783aaa05f1f499bfe6c45b4692232d4b961d7b
vini1994 <vinirabaioli94@gmail.com>
2026-03-01 18:08:51 -0300

    adiciona suporte a testes com timeout e refatora lógica de espera pelos serviços

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,7 @@ const createJestConfig = nextJest({
 });
 const JestConfig = createJestConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
+  testTimeout: 60000,
 });
 
 module.exports = JestConfig;
